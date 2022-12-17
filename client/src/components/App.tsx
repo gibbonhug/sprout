e1e6fbf9ee7c8d0b8fe31e94aef58207826dbb05
Julian <juliangwillis@gmail.com>
2022-12-17 16:20:02 -0500

    Add SCSS, new scss files, restructure /client, delete old css

---
+++ client/src/components/App.tsx
@@ -0,0 +1,13 @@
+import '../scss/react_components/App.scss'
+
+function App() {
+
+  return (
+    <div className="App">
+      Hello, World
+      <div className="child">My world... my way...</div>
+    </div>
+  )
+}
+
+export default App
