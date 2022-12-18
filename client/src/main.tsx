03dbd5b3831781fbf89b41ae7be0c4b028412b37
Julian <juliangwillis@gmail.com>
2022-12-17 19:34:18 -0500

    Go mod tidy

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './components/App'
 import './scss/main.scss';
 
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
