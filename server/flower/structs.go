9a3c03698c008bcd87de3d8f49a08eb680c2174e
Julian <juliangwillis@gmail.com>
2022-12-19 17:44:20 -0500

    Organize Go code by creating new package for flowers, and storing dummy data in dummy.go

---
+++ server/flower/structs.go
@@ -0,0 +1,32 @@
+/*
+	Package implements functions and structs for flower-based functions
+	ie Flowers, flower boxes, relationships
+*/
+package flower
+
+// Flowers
+type Flower struct {
+	ID         uint    `json:"id"` // ID of the flower
+	ColorPetal string `json:"colorPetal"` // Hex color
+}
+
+// Boxes flowers are stored in
+type Box struct {
+	ID uint `json:"id"` // ID of the box
+	Flower *Flower `json:"flower"` // Pointer to the Flower it contains
+}
+
+// Cloning a flower producing a child
+type CloneRelationship struct {
+	ID uint `json:"ID"` // ID of this relationship
+	ParentID uint `json:"parentID"` // ID of the parent flower
+	ChildID uint `json:"childID"` // ID of the child flower
+}
+
+// Breeding 2 flowers producing a child
+type BreedRelationship struct {
+	ID uint `json:"ID"` // ID of this relationship
+	Parent1ID uint `json:"parent1ID"` // ID of instigator parent flower
+	Parent2ID uint `json:"parent2ID"` // ID of the receiver parent flower
+	ChildID uint `json:"childID"` // ID of the child flower
+}
--- server/main.go
+++ server/main.go
@@ -9,64 +9,24 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-// Flowers
-type Flower struct {
-	ID         uint    `json:"id"` // ID of the flower
-	ColorPetal string `json:"colorPetal"` // Hex color
-}
-
-// Boxes flowers are stored in
-type Box struct {
-	ID uint `json:"id"` // ID of the box
-	Flower *Flower `json:"flower"` // Pointer to the Flower it contains
-}
-
-// Cloning a flower producing a child
-type CloneRelationship struct {
-	ID uint `json:"ID"` // ID of this relationship
-	ParentID uint `json:"parentID"` // ID of the parent flower
-	ChildID uint `json:"childID"` // ID of the child flower
-}
-
-// Breeding 2 flowers producing a child
-type BreedRelationship struct {
-	ID uint `json:"ID"` // ID of this relationship
-	Parent1ID uint `json:"parent1ID"` // ID of instigator parent flower
-	Parent2ID uint `json:"parent2ID"` // ID of the receiver parent flower
-	ChildID uint `json:"childID"` // ID of the child flower
-}
-
-// Dummy flowers to delete later
-var flowers = []Flower {
-	{ColorPetal: "000000", ID: 0}, {ColorPetal: "ff0000", ID: 1},
-	{ColorPetal: "800000", ID: 2},
-}
-
-// Dummy boxes
-var boxes = []Box {
-	{ID: 0, Flower: &flowers[0]}, {ID: 1, Flower: &flowers[1]}, {ID: 2, Flower: &flowers[2]}, {ID: 3},
-	{ID: 4}, {ID: 5}, {ID: 6}, {ID: 7},
-}
-
-// Dummy relationships
-var relationships = []BreedRelationship {
-	{Parent1ID: 0, Parent2ID: 1, ChildID: 2},
-}
-
+// Func creates a server on port 3000 with chi,
+// serving the dummy data defined in dummy.go on various GET endpoints.
+// This is done because database for this application does not exist yet, to be able to have basic functionality
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
+	// / GET endpoint: test if "hello" displays on screen
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("hello"))
 	})
 
-	// Boxes GET endpoint: Return JSON of all Boxes
+	// /boxes GET endpoint: Return JSON of all Boxes
 	r.Get("/boxes", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:5173")
 
-		jsonBoxes, err := json.Marshal(boxes)
+		jsonBoxes, err := json.Marshal(Boxes)
 
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
@@ -75,12 +35,12 @@ func main() {
 		w.Write(jsonBoxes)
 	})
 
-	// flowers GET endpoint: Return JSON of all flowers
+	// /flowers GET endpoint: Return JSON of all flowers
 	r.Get("/flowers", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:5173")
 
-		jsonFlowers, err := json.Marshal(flowers)
+		jsonFlowers, err := json.Marshal(Flowers)
 
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
@@ -94,7 +54,7 @@ func main() {
 		w.Header().Set("Content-Type", "application/json")
 		w.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:5173")
 
-		jsonRlns, err := json.Marshal(relationships)
+		jsonRlns, err := json.Marshal(Relationships)
 
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
@@ -106,5 +66,4 @@ func main() {
 	fmt.Println("Serving on port 3000")
 
 	http.ListenAndServe(":3000", r)
-
 }
