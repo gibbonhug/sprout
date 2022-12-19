cfa17f06295cd16a5f330d343ecc2c97eeb884c4
Julian <juliangwillis@gmail.com>
2022-12-19 12:41:57 -0500

    Work on Flower and Box structs

--- server/main.go
+++ server/main.go
@@ -11,14 +11,25 @@ import (
 
 type Flower struct {
 	ColorPetal string `json:"colorPetal"` // Hex color
-	ID         int    `json:"id"`
+	ID         uint    `json:"id"`
 	ParentID   []uint `json:"parentID"` // Flowers may have 0,1,2, or more parents
 	ChildID    []uint `json:"childID"`
 }
 
+type Box struct {
+	ID uint `json:"id"`
+	FlowerID uint `json:"box_flower"`
+}
+
 // Dummy flowers to delete later
-var flowers = []Flower{
+var flowers = []Flower {
 	{ColorPetal: "000000", ID: 0}, {ColorPetal: "ff0000", ID: 1},
+}
+
+// Dummy boxes
+var boxes = []Box {
+	{ID: 0, Flower: &flowers[0]}, {ID: 1, Flower: &flowers[1]}, {ID: 2}, {ID: 3},
+	{ID: 4}, {ID: 5}, {ID: 6}, {ID: 7},
 }
 
 func main() {
@@ -28,6 +39,22 @@ func main() {
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("hello"))
 	})
+
+	// Boxes GET endpoint: Return JSON of all Boxes
+	r.Get("/boxes", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:5173")
+
+		jsonBoxes, err := json.Marshal(boxes)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		}
+
+		w.Write(jsonBoxes)
+	})
+
+
 	// flowers GET endpoint: Return JSON of all flowers
 	r.Get("/flowers", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
