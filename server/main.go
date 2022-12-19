03764d9c3abad09e97836824f826cb337be9b3c0
Julian <juliangwillis@gmail.com>
2022-12-19 13:16:33 -0500

    Add missing ID key to relationship structs, dummy hardcoded flower relationship added

--- server/main.go
+++ server/main.go
@@ -23,12 +23,14 @@ type Box struct {
 
 // Cloning a flower producing a child
 type CloneRelationship struct {
+	ID uint `json:"ID"` // ID of this relationship
 	ParentID uint `json:"parentID"` // ID of the parent flower
 	ChildID uint `json:"childID"` // ID of the child flower
 }
 
 // Breeding 2 flowers producing a child
 type BreedRelationship struct {
+	ID uint `json:"ID"` // ID of this relationship
 	Parent1ID uint `json:"parent1ID"` // ID of instigator parent flower
 	Parent2ID uint `json:"parent2ID"` // ID of the receiver parent flower
 	ChildID uint `json:"childID"` // ID of the child flower
@@ -37,12 +39,18 @@ type BreedRelationship struct {
 // Dummy flowers to delete later
 var flowers = []Flower {
 	{ColorPetal: "000000", ID: 0}, {ColorPetal: "ff0000", ID: 1},
+	{ColorPetal: "800000", ID: 2},
 }
 
 // Dummy boxes
 var boxes = []Box {
-	{ID: 0, Flower: &flowers[0]}, {ID: 1, Flower: &flowers[1]}, {ID: 2}, {ID: 3},
+	{ID: 0, Flower: &flowers[0]}, {ID: 1, Flower: &flowers[1]}, {ID: 2, Flower: &flowers[2]}, {ID: 3},
 	{ID: 4}, {ID: 5}, {ID: 6}, {ID: 7},
+}
+
+// Dummy relationships
+var relationships = []BreedRelationship {
+	{Parent1ID: 0, Parent2ID: 1, ChildID: 2},
 }
 
 func main() {
@@ -67,7 +75,6 @@ func main() {
 		w.Write(jsonBoxes)
 	})
 
-
 	// flowers GET endpoint: Return JSON of all flowers
 	r.Get("/flowers", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
@@ -81,6 +88,21 @@ func main() {
 
 		w.Write(jsonFlowers)
 	})
+
+	// /relationships GET endpoint: Return JSON of all relationships
+	r.Get("/relationships", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:5173")
+
+		jsonRlns, err := json.Marshal(relationships)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		}
+
+		w.Write(jsonRlns)
+	})
+
 
 	http.ListenAndServe(":3000", r)
 
