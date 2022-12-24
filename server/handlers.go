b107b614511439defdd3013ec97d2abfe8882cf0
Julian <juliangwillis@gmail.com>
2022-12-23 20:23:28 -0500

    Move callback HTTP handler functions to new handlers.go file in package main

---
+++ server/handlers.go
@@ -0,0 +1,283 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/gibbonhug/sprout/flower"
+	"github.com/go-chi/chi/v5"
+)
+
+// For local JSON GET requests
+// Set content-type and access-control-allow-origin headers
+func setLocalJSONHeaders(w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:5173")
+}
+
+func GetHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello world"))
+}
+
+func GetBoxes(w http.ResponseWriter, r *http.Request) {
+	setLocalJSONHeaders(w)
+
+	boxes, err := ioutil.ReadFile("./data/boxes.json")
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+
+	w.Write(boxes)
+}
+
+func GetBoxesParam(w http.ResponseWriter, r *http.Request) {
+	setLocalJSONHeaders(w)
+
+	// Grab url param
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+
+	// Get JSON data
+	boxJSON, err := ioutil.ReadFile("./data/boxes.json")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+
+	// Unmarshal JSON data to boxSlice
+	var boxSlice []*flower.Box
+
+	err = json.Unmarshal(boxJSON, &boxSlice)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+
+	// Loop through flowers to find one with this ID
+
+	var returnData []byte
+
+	for _, box := range boxSlice {
+		if box.ID == uint(id) {
+			returnData, err = json.Marshal(box)
+
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+			}
+
+			w.Write(returnData)
+			return
+		}
+	}
+
+	// Box with this ID does not exist
+	http.Error(w, "Data does not exist", http.StatusNotFound)
+}
+
+func GetFlowers(w http.ResponseWriter, r *http.Request) {
+	setLocalJSONHeaders(w)
+
+	flowers, err := ioutil.ReadFile("./data/flowers.json")
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+
+	w.Write(flowers)
+}
+
+func GetFlowersParam(w http.ResponseWriter, r *http.Request) {
+	setLocalJSONHeaders(w)
+
+	// Grab url param
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+
+	// Get JSON data
+	flowersJSON, err := ioutil.ReadFile("./data/flowers.json")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+
+	// Unmarshal JSON data to flowersSlice
+	var flowersSlice []*flower.Flower
+
+	err = json.Unmarshal(flowersJSON, &flowersSlice)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+
+	// Loop through flowers to find one with this ID
+
+	var returnData []byte
+
+	for _, flower := range flowersSlice {
+		if flower.ID == uint(id) {
+			returnData, err = json.Marshal(flower)
+
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+			}
+
+			w.Write(returnData)
+			return
+		}
+	}
+
+	// Flower with this ID does not exist
+	http.Error(w, "Data does not exist", http.StatusNotFound)
+}
+
+func PostFlowers(w http.ResponseWriter, r *http.Request) {
+	// Decode JSON data sent in this request, as a Flower
+	decoder := json.NewDecoder(r.Body)
+	var f flower.Flower // The data as a flower
+	err := decoder.Decode(&f)
+	if err != nil {
+		http.Error(w, "something bad happen", http.StatusBadRequest)
+	}
+
+	// Get old JSON data
+	oldFlowersJSON, err := ioutil.ReadFile("./data/flowers.json")
+	if err != nil {
+		http.Error(w, "something bad happen", http.StatusBadRequest)
+	}
+
+	// Unmarshall old JSON data into 'flowers'
+	var flowers []flower.Flower
+	err = json.Unmarshal(oldFlowersJSON, &flowers)
+	if err != nil {
+		http.Error(w, "something bad happen", http.StatusBadRequest)
+	}
+
+	// Append data in f (posted data) into flowers
+	flowers = append(flowers, f)
+
+	// convert flowers to json
+	flowersJSON, _ := json.Marshal(flowers)
+
+	// Update temp JSON database
+	err = os.WriteFile("./data/flowers.json", flowersJSON, 0777)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func GetPairRlns(w http.ResponseWriter, r *http.Request) {
+	setLocalJSONHeaders(w)
+
+	prlns, err := ioutil.ReadFile("./data/pairrelationships.json")
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+
+	w.Write(prlns)
+}
+
+func GetPairRlnsParam(w http.ResponseWriter, r *http.Request) {
+	setLocalJSONHeaders(w)
+
+	// Grab url param
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+
+	// Get JSON data
+	pairJSON, err := ioutil.ReadFile("./data/pairrelationships.json")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+
+	// Unmarshal JSON data to pairSlice
+	var pairSlice []*flower.PairRln
+
+	err = json.Unmarshal(pairJSON, &pairSlice)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+
+	// Loop through flowers to find one with this ID
+
+	var returnData []byte
+
+	for _, pair := range pairSlice {
+		if pair.ID == uint(id) {
+			returnData, err = json.Marshal(pair)
+
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+			}
+
+			w.Write(returnData)
+			return
+		}
+	}
+
+	// Pair with this ID does not exist
+	http.Error(w, "Data does not exist", http.StatusNotFound)
+}
+
+func GetCloneRlns(w http.ResponseWriter, r *http.Request) {
+	setLocalJSONHeaders(w)
+
+	crlns, err := ioutil.ReadFile("./data/clonerelationships.json")
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+
+	w.Write(crlns)
+}
+
+func GetCloneRlnsParam(w http.ResponseWriter, r *http.Request) {
+	setLocalJSONHeaders(w)
+
+	// Grab url param
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+
+	// Get JSON data
+	cloneJSON, err := ioutil.ReadFile("./data/clonerelationships.json")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+
+	// Unmarshal JSON data to cloneSlice
+	var cloneSlice []*flower.CloneRln
+
+	err = json.Unmarshal(cloneJSON, &cloneSlice)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+
+	// Loop through clones to find one with this ID
+
+	var returnData []byte
+
+	for _, clone := range cloneSlice {
+		if clone.ID == uint(id) {
+			returnData, err = json.Marshal(clone)
+
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+			}
+
+			w.Write(returnData)
+			return
+		}
+	}
+
+	// Clone with this ID does not exist
+	http.Error(w, "Data does not exist", http.StatusNotFound)
+}
--- server/main.go
+++ server/main.go
@@ -1,320 +1,63 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"log"
 	"net/http"
-	"os"
-	"strconv"
-
-	"github.com/gibbonhug/sprout/flower"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-// For local JSON GET requests
-// Set content-type and access-control-allow-origin headers
-func setLocalJSONHeaders(w http.ResponseWriter) {
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:5173")
-}
-
 // Func creates a server on port 3000 with chi,
 // serving the dummy data defined in dummy.go on various GET endpoints.
-// This is done because database for this application does not exist yet, 
+// This is done because database for this application does not exist yet,
 // to be able to have basic functionality while developing
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
-	// / GET endpoint: test if "hello" displays on screen
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello"))
-	})
+	/*
+		ENDPOINTS
+	*/
+
+	// / GET endpoint: test if "hello world" displays on screen
+	r.Get("/", GetHome)
 
 	// /boxes GET endpoint: Return JSON of all Boxes
-	r.Get("/boxes", func(w http.ResponseWriter, r *http.Request) {
-		setLocalJSONHeaders(w)
-
-		boxes, err := ioutil.ReadFile("./data/boxes.json")
-
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-		}
-
-		w.Write(boxes)
-	})
+	r.Get("/boxes", GetBoxes)
 
 	// /boxes/{id} GET endpoint: Return box with this id
 	// TEMPORARY CODE
-	r.Get("/boxes/{id}", func(w http.ResponseWriter, r *http.Request) {
-		setLocalJSONHeaders(w)
-
-		// Grab url param
-		id, err := strconv.Atoi(chi.URLParam(r, "id"))
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-		}
-
-		// Get JSON data
-		boxJSON, err := ioutil.ReadFile("./data/boxes.json")
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-		}
-
-		// Unmarshal JSON data to boxSlice
-		var boxSlice []*flower.Box
-
-		err = json.Unmarshal(boxJSON, &boxSlice)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-		}
-
-		// Loop through flowers to find one with this ID
-
-		var returnData []byte
-
-		for _, box := range boxSlice {
-			if box.ID == uint(id) {
-				returnData, err = json.Marshal(box)
-
-				if err != nil {
-					http.Error(w, err.Error(), http.StatusBadRequest)
-				}
-
-				w.Write(returnData)
-				return
-			}
-		}
-
-		// Box with this ID does not exist
-		http.Error(w, "Data does not exist", http.StatusNotFound)
-
-	})
-
+	r.Get("/boxes/{id}", GetBoxesParam)
 
 	// /flowers GET endpoint: Return JSON of all flowers
-	r.Get("/flowers", func(w http.ResponseWriter, r *http.Request) {
-		setLocalJSONHeaders(w)
-
-		flowers, err := ioutil.ReadFile("./data/flowers.json")
-
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-		}
-
-		w.Write(flowers)
-	})
+	r.Get("/flowers", GetFlowers)
 
 	// /flowers/{id} GET endpoint: Return flower with this id
 	// TEMPORARY CODE
-	r.Get("/flowers/{id}", func(w http.ResponseWriter, r *http.Request) {
-		setLocalJSONHeaders(w)
-
-		// Grab url param
-		id, err := strconv.Atoi(chi.URLParam(r, "id"))
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-		}
-
-		// Get JSON data
-		flowersJSON, err := ioutil.ReadFile("./data/flowers.json")
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-		}
-
-		// Unmarshal JSON data to flowersSlice
-		var flowersSlice []*flower.Flower
-
-		err = json.Unmarshal(flowersJSON, &flowersSlice)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-		}
-
-		// Loop through flowers to find one with this ID
-
-		var returnData []byte
-
-		for _, flower := range flowersSlice {
-			if flower.ID == uint(id) {
-				returnData, err = json.Marshal(flower)
-
-				if err != nil {
-					http.Error(w, err.Error(), http.StatusBadRequest)
-				}
-
-				w.Write(returnData)
-				return
-			}
-		}
-
-		// Flower with this ID does not exist
-		http.Error(w, "Data does not exist", http.StatusNotFound)
-
-	})
+	r.Get("/flowers/{id}", GetFlowersParam)
 
 	// /flowers POST endpoint: Create a flower and update JSON file
-	r.Post("/flowers", func(w http.ResponseWriter, r *http.Request) {
-		// Decode JSON data sent in this request, as a Flower
-		decoder := json.NewDecoder(r.Body)
-		var f flower.Flower // The data as a flower
-		err := decoder.Decode(&f)
-		if err != nil {
-			http.Error(w, "something bad happen", http.StatusBadRequest)
-		}
-
-		// Get old JSON data
-		oldFlowersJSON, err := ioutil.ReadFile("./data/flowers.json")
-		if err != nil {
-			http.Error(w, "something bad happen", http.StatusBadRequest)
-		}
-
-		// Unmarshall old JSON data into 'flowers'
-		var flowers []flower.Flower
-		err = json.Unmarshal(oldFlowersJSON, &flowers)
-		if err != nil {
-			http.Error(w, "something bad happen", http.StatusBadRequest)
-		}
-
-		// Append data in f (posted data) into flowers
-		flowers = append(flowers, f)
-
-		// convert flowers to json
-		flowersJSON, _ := json.Marshal(flowers)
-
-		// Update temp JSON database
-		err = os.WriteFile("./data/flowers.json", flowersJSON, 0777)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-	})
+	r.Post("/flowers", PostFlowers)
 
 	// /pairrlns GET endpoint: Return JSON of all pair relationships
-	r.Get("/pairrlns", func(w http.ResponseWriter, r *http.Request) {
-		setLocalJSONHeaders(w)
-
-		prlns, err := ioutil.ReadFile("./data/pairrelationships.json")
-
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-		}
-
-		w.Write(prlns)
-	})
+	r.Get("/pairrlns", GetPairRlns)
 
 	// /pairrlns/{id} GET endpoint: Return pair rln with this id
 	// TEMPORARY CODE
-	r.Get("/pairrlns/{id}", func(w http.ResponseWriter, r *http.Request) {
-		setLocalJSONHeaders(w)
-
-		// Grab url param
-		id, err := strconv.Atoi(chi.URLParam(r, "id"))
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-		}
-
-		// Get JSON data
-		pairJSON, err := ioutil.ReadFile("./data/pairrelationships.json")
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-		}
-
-		// Unmarshal JSON data to pairSlice
-		var pairSlice []*flower.PairRln
-
-		err = json.Unmarshal(pairJSON, &pairSlice)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-		}
-
-		// Loop through flowers to find one with this ID
-
-		var returnData []byte
-
-		for _, pair := range pairSlice {
-			if pair.ID == uint(id) {
-				returnData, err = json.Marshal(pair)
-
-				if err != nil {
-					http.Error(w, err.Error(), http.StatusBadRequest)
-				}
-
-				w.Write(returnData)
-				return
-			}
-		}
-
-		// Pair with this ID does not exist
-		http.Error(w, "Data does not exist", http.StatusNotFound)
-
-	})
+	r.Get("/pairrlns/{id}", GetPairRlnsParam)
 	
-
 	// /clonerlns GET endpoint: Return JSON of all clone relationships
-	r.Get("/clonerlns", func(w http.ResponseWriter, r *http.Request) {
-		setLocalJSONHeaders(w)
+	r.Get("/clonerlns", GetCloneRlns)
 
-		crlns, err := ioutil.ReadFile("./data/clonerelationships.json")
-
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-		}
-
-		w.Write(crlns)
-	})
-
-	// /clonerlns/{id} GET endpoint: Return flower with this id
+	// /clonerlns/{id} GET endpoint: Return clone rln with this id
 	// TEMPORARY CODE
-	r.Get("/clonerlns/{id}", func(w http.ResponseWriter, r *http.Request) {
-		setLocalJSONHeaders(w)
+	r.Get("/clonerlns/{id}", GetCloneRlnsParam)
 
-		// Grab url param
-		id, err := strconv.Atoi(chi.URLParam(r, "id"))
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-		}
-
-		// Get JSON data
-		cloneJSON, err := ioutil.ReadFile("./data/clonerelationships.json")
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-		}
-
-		// Unmarshal JSON data to cloneSlice
-		var cloneSlice []*flower.CloneRln
-
-		err = json.Unmarshal(cloneJSON, &cloneSlice)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-		}
-
-		// Loop through clones to find one with this ID
-
-		var returnData []byte
-
-		for _, clone := range cloneSlice {
-			if clone.ID == uint(id) {
-				returnData, err = json.Marshal(clone)
-
-				if err != nil {
-					http.Error(w, err.Error(), http.StatusBadRequest)
-				}
-
-				w.Write(returnData)
-				return
-			}
-		}
-
-		// Clone with this ID does not exist
-		http.Error(w, "Data does not exist", http.StatusNotFound)
-
-	})
-
-
+	/*
+		END ENDPOINTS
+	*/
+	
 	fmt.Println("Serving on port 3000")
 
 	http.ListenAndServe(":3000", r)
